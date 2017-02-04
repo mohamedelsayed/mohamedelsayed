f6c7148455b9c1f93dfd1001371d6255e00a7d24
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2017-02-04 04:46:47 +0200

    shift

--- sites/all/themes/front/templates/home/contact.php
+++ sites/all/themes/front/templates/home/contact.php
@@ -1,46 +1,101 @@
-<?php global $base_url;?>
-<div id="contact" class="section-padding">
-	<div class="container">
-		<div class="row">
-			<?php if(isset($home_widgets['contact']) && !empty($home_widgets['contact'])){
-			$item = $home_widgets['contact'];
-			$title = '';$body = '';
-			if(isset($item->title)){
-				$title = $item->title;
-			}	
-			if(isset($item->body[LANGUAGE_NONE][0]['value'])){
-				$body = $item->body[LANGUAGE_NONE][0]['value'];
-			}?>
-				<div class="page-title text-center">
-					<h1><?php echo $title;?></h1>
-					<p><?php echo $body;?></p>
-					<hr class="pg-titl-bdr-btm"></hr>
-				</div>
-			<?php }?>
-            <div id="sendmessage"><?php echo __('شكرا لرسالتك. وسوف نعود اليكم في أقرب وقت ممكن.');?></div>
-            <div id="errormessage"><?php echo __('هناك مشكلة في إرسال البريد الإلكتروني. حاول مرة اخرى.');?></div>            
-			<div class="form-sec">
-            	<form id="contact-form" onsubmit="return validate_contact_us_form();" action="<?php echo $base_url.'/contact-us-form';?>" method="post" role="form" class="contactForm">
-                	<div class="col-md-4 form-group">
-                        <input type="text" name="name" id="name" class="form-control text-field-box" placeholder="<?php echo __('الاسم');?>" />
-                        <div class="validation"></div>
-                    </div>
-                    <div class="col-md-4 form-group">
-                        <input type="text" class="form-control text-field-box" name="email" id="email" placeholder="<?php echo __('البريد الالكتروني');?>"  />
-                        <div class="validation"></div>
-                    </div>
-                    <div class="col-md-4 form-group">
-                        <input type="text" class="form-control text-field-box" name="subject" id="subject" placeholder="<?php echo __('موضوع');?>"  />
-                        <div class="validation"></div>
-                    </div>
-                    <div class="col-md-12 form-group">
-                        <textarea class="form-control text-field-box" name="message" id="message" rows="5"  placeholder="<?php echo __('الرسالة');?>"></textarea>
-                        <div class="validation"></div> 
-                        <div id="sendmail_ajaxLoading"></div>       
-                        <button class="button-medium" id="contact-submit" type="submit" name="contact"><?php echo __('ارسال');?></button>
-                    </div>
-                </form>
-            </div>
+<div id="get-touch">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+						<div class="get-touch-heading">
+							<h2>get in touch</h2> 
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent metus magna,malesuada porta elementum vitae.</p>
+						</div>
+					</div>
+				</div>   
+
+				<div class="content">
+	                <div class="row">
+                        <div id="sendmessage">Your message has been sent. Thank you!</div>
+                        <div id="errormessage"></div>
+                        
+                         <form action="" method="post" role="form" class="form contactForm">
+	                        <div class="col-md-4">
+	                            <div class="form-group">
+                                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                    <div class="validation"></div>
+                                </div>
+	                        </div>
+	                        <div class="col-md-4">
+	                            <div class="form-group">
+                                    <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                                    <div class="validation"></div>
+                                </div>
+	                        </div>
+	                        <div class="col-md-4">
+	                            <div class="form-group">
+                                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                                    <div class="validation"></div>
+                                </div>
+	                        </div>
+	                        <div class="col-md-12">
+	                            <div class="form-group">
+                                    <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                                    <div class="validation"></div>
+                                </div>
+	                        </div>
+	                        <div class="submit">
+	                            <button class="btn btn-default" type="submit">Send Now</button>
+	                        </div>
+	                    </form>
+	                </div>
+            </div>	
+	    	</div>
 		</div>
-	</div>
-</div>
+
+
+		<!--contact-->
+		<div id="contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+						<div class="contact-heading">
+							<h2>contact</h2> 
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent metus magna,malesuada porta elementum vitae.</p>
+						</div>
+					</div>
+				</div>   	
+	    	</div>
+
+	    	<div id="google-map" data-latitude="40.713732" data-longitude="-74.0092704"></div>
+            
+		</div>
+
+
+		<!--client-->
+		<div id="client">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-sm-4 col-md-2">
+						<span></span><img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/client/client1.png" alt="">
+					</div>
+
+					<div class="col-sm-4 col-md-2">
+						<span></span><img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/client/client2.png" alt="">
+					</div>
+
+					<div class="col-sm-4 col-md-2">
+						<span></span><img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/client/client3.png" alt="">
+					</div>
+
+					<div class="col-sm-4 col-md-2">
+						<span></span><img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/client/client4.png" alt="">
+					</div>
+
+					<div class="col-sm-4 col-md-2">
+						<span></span><img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/client/client5.png" alt="">
+					</div>
+
+					<div class="col-sm-4 col-md-2">
+						<span></span><img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/client/client6.png" alt="">
+					</div>
+
+				</div>
+			</div>
+		</div>
