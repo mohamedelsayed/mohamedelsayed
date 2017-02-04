3db507cdcbaee8de055f90a457efc11f597fc93a
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2017-02-05 00:38:56 +0200

    shift

--- sites/all/themes/front/templates/common/footer.php
+++ sites/all/themes/front/templates/common/footer.php
@@ -1,79 +1,78 @@
 <div id="footer">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4">
-						<div class="footer-heading">
-							<h3><span>about</span> us</h3>
-							<p>To explore strange new worlds to seek out new life and new civilizations to boldly go where no man has gone before. It's time to play the music.</p>
-							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-						</div>
-					</div>
-
-					<div class="col-md-4">
-						<div class="footer-heading">
-							<h3><span>latest</span> news</h3>
-							<ul>
-								<li><a href="#">Trends don't matter, but techniques do</a></li>
-								<li><a href="#">Trends don't matter, but techniques do</a></li>
-								<li><a href="#">Trends don't matter, but techniques do</a></li>
-								<li><a href="#">Trends don't matter, but techniques do</a></li>
-							</ul>
-						</div>
-					</div>
-
-					<div class="col-md-4">
-						<div class="footer-heading">
-							<h3><span>follow</span> us on instagram</h3>
-							<div class="insta">
-								<ul>
-									<img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/footer/footer1.jpg" alt="">
-									<img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/footer/footer2.jpg" alt="">
-									<img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/footer/footer3.jpg" alt="">
-									<img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/footer/footer4.jpg" alt="">
-									<img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/footer/footer5.jpg" alt="">
-									<img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/footer/footer6.jpg" alt="">
-								</ul>
-							</div>
-						</div>
-					</div>
-
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="footer-heading">
+					<h3><span>about</span> us</h3>
+					<p>To explore strange new worlds to seek out new life and new civilizations to boldly go where no man has gone before. It's time to play the music.</p>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 				</div>
 			</div>
-		</div>
 
-		<!--bottom footer-->
-		<div id="bottom-footer" class="hidden-xs">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4">
-						<div class="footer-left">
-							&copy; MyBix Theme. All rights reserved
-                            <div class="credits">
-                                <!-- 
-                                    All the links in the footer should remain intact. 
-                                    You can delete the links only if you purchased the pro version.
-                                    Licensing information: https://bootstrapmade.com/license/
-                                    Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=MyBiz
-                                -->
-                                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-                            </div>
-						</div>
-					</div>
+			<div class="col-md-4">
+				<div class="footer-heading">
+					<h3><span>latest</span> news</h3>
+					<ul>
+						<li><a href="#">Trends don't matter, but techniques do</a></li>
+						<li><a href="#">Trends don't matter, but techniques do</a></li>
+						<li><a href="#">Trends don't matter, but techniques do</a></li>
+						<li><a href="#">Trends don't matter, but techniques do</a></li>
+					</ul>
+				</div>
+			</div>
 
-					<div class="col-md-8">
-						<div class="footer-right">
-                            <ul class="list-unstyled list-inline pull-right">
-                                <li><a href="#home">Home</a></li>
-                                <li><a href="#about">About</a></li>
-                                <li><a href="#service">Service</a></li>
-                                <li><a href="#portfolo">Portfolio</a></li>
-                                <li><a href="#contact">Contact</a></li>
-                            </ul>
-						</div>
+			<div class="col-md-4">
+				<div class="footer-heading">
+					<h3><span>follow</span> us on instagram</h3>
+					<div class="insta">
+						<ul>
+							<img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/footer/footer1.jpg" alt="">
+							<img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/footer/footer2.jpg" alt="">
+							<img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/footer/footer3.jpg" alt="">
+							<img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/footer/footer4.jpg" alt="">
+							<img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/footer/footer5.jpg" alt="">
+							<img src="<?php echo $base_url.'/'.path_to_theme().'/images';?>/footer/footer6.jpg" alt="">
+						</ul>
 					</div>
 				</div>
 			</div>
+
 		</div>
+	</div>
+</div>
+<!--bottom footer-->
+<div id="bottom-footer" class="hidden-xs">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="footer-left">
+					&copy; MyBix Theme. All rights reserved
+                    <div class="credits">
+                        <!-- 
+                            All the links in the footer should remain intact. 
+                            You can delete the links only if you purchased the pro version.
+                            Licensing information: https://bootstrapmade.com/license/
+                            Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=MyBiz
+                        -->
+                        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                    </div>
+				</div>
+			</div>
+
+			<div class="col-md-8">
+				<div class="footer-right">
+                    <ul class="list-unstyled list-inline pull-right">
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#service">Service</a></li>
+                        <li><a href="#portfolo">Portfolio</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <div id="Developer">
     Developed by <a href="http://www.mohamedelsayed.net" target="_blank">Mohamed Elsayed</a>
 </div>
