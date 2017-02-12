4065cc77f4bdbcd7032f2e5db1f651eb35a2dc5a
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2017-02-12 02:53:35 +0200

    shift

--- sites/all/themes/front/templates/page--front.tpl.php
+++ sites/all/themes/front/templates/page--front.tpl.php
@@ -4,7 +4,6 @@ $home_widgets = elsayed_get_home_widgets();
 include_once 'common'.DS.'header.php';
 include_once 'home'.DS.'slider.php';
 include_once 'home'.DS.'about.php';
-//include_once 'home'.DS.'service.php';
 include_once 'home'.DS.'portfolio.php';
 include_once 'home'.DS.'contact.php';		
 include_once 'common'.DS.'footer.php';?>
