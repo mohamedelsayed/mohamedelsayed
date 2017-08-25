1a0873c0d5c0e4c1cab6355051e7745f7c78a4de
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2017-08-25 23:11:50 +0200

    shift

--- sites/all/themes/front/template.php
+++ sites/all/themes/front/template.php
@@ -1,60 +1,44 @@
-<?php /**
- * @author Author "Mohamed Elsayed"  
+<?php
+
+/**
+ * @author Author "Mohamed Elsayed"
  * @author Author Email "me@mohamedelsayed.net"
- * @link http://www.mohamedelsayed.net
- * @copyright Copyright (c) 2015 Programming by "mohamedelsayed.net"
+ * @link http://www.themwl.org/
+ * @copyright Copyright (c) 2017 Programming by "http://www.mohamedelsayed.net"
  */
 function front_theme() {
     $items = array();
-    /*$items['user_login'] = array(
-        'render element' => 'form',
-        'path' => drupal_get_path('theme', 'front') . '/templates',
-        'template' => 'user-login',
-    );
-    $items['user_register_form'] = array(
-        'render element' => 'form',
-        'path' => drupal_get_path('theme', 'front') . '/templates',
-        'template' => 'user-register-form',
-    );
-    $items['user_pass'] = array(
-        'render element' => 'form',
-        'path' => drupal_get_path('theme', 'front') . '/templates',
-        'template' => 'user-pass',
-    );
-    $items['user_profile_form'] = array(
-        'render element' => 'form',
-        'path' => drupal_get_path('theme', 'front') . '/templates',
-        'template' => 'user-profile-edit',
-    );*/
     return $items;
 }
+
 function front_form_alter(&$form, &$form_state, $form_id) {
-    //$sitelang = get_language_from_url();    
     if ($form_id == 'user_login') {
         $form['name']['#attributes']["placeholder"] = __('Username');
         $form['name']['#description'] = '';
         $form['pass']['#attributes']["placeholder"] = __('Password');
-        $form['pass']['#description'] = '';        
+        $form['pass']['#description'] = '';
     }
-    if($form_id == 'user_pass'){
+    if ($form_id == 'user_pass') {
         $form['name']['#attributes']["placeholder"] = __('Username or e-mail address');
     }
 }
+
 function front_preprocess_page(&$vars) {
     $vars['show_messages'] = TRUE;
     $vars['page_content'] = $vars['page']['content'];
-    drupal_add_library('system', 'ui.autocomplete');
+    //drupal_add_library('system', 'ui.autocomplete');
     if (isset($vars['node'])) {
         $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type;
     }
 }
+
 function front_preprocess_node(&$variables) {
     $node = $variables ['node'];
     if ($variables['view_mode'] == 'full' && node_is_page($variables['node'])) {
-
         $variables['classes_array'][] = 'node-full';
     }
 }
+
 function front_page_alter($page) {
     $viewport = array(
         '#type' => 'html_tag',
@@ -65,6 +49,7 @@ function front_page_alter($page) {
     );
     drupal_add_html_head($viewport, 'viewport');
 }
+
 function front_node_preview($variables) {
     $node = $variables['node'];
     $preview_trimmed_version = FALSE;
