813549c323fd1954f8186c25c1ed60bf415bf1ad
Mohamed Elsayed <me@mohamedelsayed.net>
2018-10-06 02:11:34 +0200

    add backup script

--- runAutoBackup.sh
+++ runAutoBackup.sh
@@ -2,6 +2,13 @@
 #This script is used to make automatic backup
 #Author: Mohamed Elsayed
 #Date: 06/10/2018
+if [ $# -eq 0  ]
+then
+    echo "Please supply database password!"
+    echo "Usage: ./runAutoBackup.sh <databasePassword>"
+    exit
+fi
+databasePassword=$1
 #make directory with current date
 currentYear=$(date +"%Y")
 currentMonth=$(date +"%m")
@@ -15,6 +22,6 @@ cd $backupPath
 #create tar.gz file for project files:
 tar -zcvf files.tar.gz $currentPath
 #backup mysql database:
-mysqldump -u elsayed_mohamede -pvTJep5pfyesE elsayed_mohamedelsayed > elsayed_mohamedelsayed.sql
+mysqldump -u elsayed_mohamede -p$databasePassword elsayed_mohamedelsayed > elsayed_mohamedelsayed.sql
 #Script tasks complete.
 echo "Backup complete in this path $backupPath."
